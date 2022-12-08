9948482bb818842316055c7bd7535131db08628a
LilianLem <54894780+LilianLem@users.noreply.github.com>
2022-12-08 18:45:14 +0100

    User homepage (WIP) + navbars and footer

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return /*$this->getUser()
+            ?*/ $this->render("home/custom.html.twig")
+            /*: $this->render("home/guest.html.twig")*/;
+    }
+}
