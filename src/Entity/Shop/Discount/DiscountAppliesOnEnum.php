8057d25c7de88c2ed7cb224bd4534ab689d8fce2
LilianLem <54894780+LilianLem@users.noreply.github.com>
2023-04-27 17:55:52 +0200

    Adding more specific AppliedDiscount types

--- src/Entity/Shop/Discount/DiscountAppliesOnEnum.php
+++ src/Entity/Shop/Discount/DiscountAppliesOnEnum.php
@@ -4,6 +4,8 @@ namespace App\Entity\Shop\Discount;
 
 enum DiscountAppliesOnEnum: string
 {
-    case ORDER = "order";
-    case PRODUCT = "product";
+    case ORDER = "Commande";
+    case ALL_ELIGIBLE_PRODUCTS = "Produits éligibles";
+    case CHEAPEST_ELIGIBLE_PRODUCT = "Produit éligible le moins cher";
+    case MOST_EXPENSIVE_ELIGIBLE_PRODUCT = "Produit éligible le plus cher";
 }
